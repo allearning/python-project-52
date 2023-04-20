62d1220066cb78560a4eb1d473de21b18f243cc7
AlexeyLearning <alexeylearning@gmail.com>
2023-04-20 19:58:26 +0300

    Basic index page

---
+++ task_manager/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+class HomePageView(TemplateView):
+
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+def about(request):
+    return render(request, 'about.html')
