1cc5e94634fabf8d4d66dcf828ceb68d17e08903
AlexeyLearning <alexeylearning@gmail.com>
2023-12-05 20:24:21 +0300

    add base and index page template

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.views.generic import TemplateView
 
 def index(request):
     return HttpResponse('Hello, World! 12 2023')
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
