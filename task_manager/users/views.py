ad9e9ec198ddd94bfb56f7a35a306b185099cc12
AlexeyLearning <alexeylearning@gmail.com>
2024-03-13 21:49:21 +0300

    Messages implemented in Users Index and Login pages

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,8 +1,12 @@
 from django.urls import reverse_lazy
+from django.contrib import messages
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.models import User
+from django.shortcuts import redirect
 from django.utils.translation import gettext_lazy as _
-from .forms import CreateUserForm, UpdateUserForm
+
+from task_manager import settings
+from task_manager.users.forms import CreateUserForm, UpdateUserForm
 
 # Create your views here.
 
@@ -25,10 +29,30 @@ class UserCreateView(CreateView):
     form_class = CreateUserForm
 
     success_url = reverse_lazy('login')
-    #success_message = _('User is successfully registered')
+    # success_message = _('User is successfully registered')
 
 
-class UserUpdateView(UpdateView):
+class MyUserControlMixin:
+
+    login_url = settings.LOGIN_URL
+    wrong_user_url = login_url
+
+    not_logged_message = _('You are not logged in! Please Log in.')
+    lincorrect_user_message = _('You have no rights to edit another user')
+
+    def get(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            messages.error(self.request, self.__class__.not_logged_message)
+            return redirect(self.__class__.login_url, self.request)
+
+        if self.request.user.id != self.kwargs['pk']:
+            messages.error(self.request, self.__class__.lincorrect_user_message)
+            return redirect(self.__class__.wrong_user_url, self.request)
+        
+        return super().get(self, *args, **kwargs)    
+
+
+class UserUpdateView(MyUserControlMixin, UpdateView):
     """
     Update user.
     """
@@ -37,16 +61,22 @@ class UserUpdateView(UpdateView):
 
     form_class = UpdateUserForm
 
+    login_url = reverse_lazy('login')
+    wrong_user_url = reverse_lazy('users')
+
     success_url = reverse_lazy('users')
-    #success_message = _('User is successfully registered')
+    # success_message = _('User is successfully registered')
 
 
-class UserDeleteView(DeleteView):
+class UserDeleteView(MyUserControlMixin, DeleteView):
     """
     Delete user.
     """
     template_name = "users/remove.html"
     model = User
 
+    login_url = reverse_lazy('login')
+    wrong_user_url = reverse_lazy('users')
+
     success_url = reverse_lazy('index')
-    #success_message = _('User is successfully registered')
+    # success_message = _('User is successfully registered')
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,15 +1,17 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.views  import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
-
+from django.utils.translation import gettext_lazy as _
 
 class IndexView(TemplateView):
     template_name = "index.html"
 
 
-class MyLoginView(LoginView):
+class MyLoginView(SuccessMessageMixin, LoginView):
     template_name = 'login.html'
     next_page = reverse_lazy('index')
+    success_message = _('You have logged in')
 
 
 class MyLogoutView(LogoutView):
